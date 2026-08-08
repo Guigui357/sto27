6c6449d0ddaf14ca09c1c92b3e9272c349631549
Guigui357 <zebuchinho77@gmail.com>
2026-08-08 14:48:37 -0300

    Add main.m file for iOS application entry point

---
+++ main.m
@@ -0,0 +1,11 @@
+// ================================================================
+// main.m
+// ================================================================
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
