cb28bd760b4a5124601482eaf58be1945368d051
Guigui357 <zebuchinho77@gmail.com>
2026-08-08 15:47:41 -0300

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,5 +1,5 @@
 // ================================================================
-// AppDelegate.m
+// AppDelegate.m - Interface Programática (Sem Storyboard)
 // ================================================================
 
 #import "AppDelegate.h"
@@ -8,10 +8,17 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Cria a window
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
+    // Cria o ViewController
     ViewController *vc = [[ViewController alloc] init];
-    self.window.rootViewController = vc;
+    
+    // Coloca na navigation controller (opcional, mas bom para navegação)
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    nav.navigationBarHidden = YES; // Esconde a barra de navegação
+    
+    self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     
     return YES;
