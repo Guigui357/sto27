4e250cb445cddab5bbbbaa063c5fbd20bbd066f1
Guigui357 <zebuchinho77@gmail.com>
2026-08-08 14:46:51 -0300

    Add ViewController interface with button actions

---
+++ ViewController.h
@@ -0,0 +1,11 @@
+// ================================================================
+// ViewController.h
+// ================================================================
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)scanButtonTapped:(id)sender;
+- (IBAction)exportButtonTapped:(id)sender;
+- (IBAction)clearLogButtonTapped:(id)sender;
+@end
